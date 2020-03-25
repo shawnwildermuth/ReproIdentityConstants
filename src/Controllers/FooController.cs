8ca494024588e7e7adc68868be8d58f6244c7e8a
Shawn Wildermuth <shawn@wildermuth.com>
2020-03-25 00:12:02 -0400

    Reproduction Example

---
+++ src/Controllers/FooController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReproIdentityConstants.Controllers
+{
+  [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+  [Route("api/[controller]")]
+  public class FooController : ControllerBase
+  {
+    public IActionResult Get()
+    {
+      return Ok(new { name = "Hello" });
+    }
+  }
+}
